b195a7246da00c94cb4b2830039a82db6b6d3591
oybek703 <hhoybek@gmail.com>
2021-05-26 01:21:40 +0500

    Updates

---
+++ precache-manifest.6a2c2a1e1ee13c21447a3a66cd43477e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "64cdc080c79cdd0ce95c",
+    "url": "/oybek703/robofriends/static/css/main.00bba3e0.chunk.css"
+  },
+  {
+    "revision": "64cdc080c79cdd0ce95c",
+    "url": "/oybek703/robofriends/static/js/main.64cdc080.chunk.js"
+  },
+  {
+    "revision": "22483e599d22605dd598",
+    "url": "/oybek703/robofriends/static/css/1.28db05d7.chunk.css"
+  },
+  {
+    "revision": "22483e599d22605dd598",
+    "url": "/oybek703/robofriends/static/js/1.22483e59.chunk.js"
+  },
+  {
+    "revision": "e733630f669c7793bb84",
+    "url": "/oybek703/robofriends/static/js/runtime~main.e733630f.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/oybek703/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1f9b611ef57d007230152e8c72838b08",
+    "url": "/oybek703/robofriends/index.html"
+  }
+];
